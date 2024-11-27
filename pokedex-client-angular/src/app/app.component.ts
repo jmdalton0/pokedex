40e17115f9810c5780341a7546364b7cf4d8ecb3
Jesse Dalton <jmdalton0@gmail.com>
2024-11-27 14:37:19 -0700

    save before removing images from db?

--- pokedex-client-angular/src/app/app.component.ts
+++ pokedex-client-angular/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		RouterLinkActive,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pokedex-client-angular';
+	title = 'Pokedex';
 }
--- pokedex-client-angular/src/app/app.routes.ts
+++ pokedex-client-angular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pokemon', component: PokemonListComponent },
+];
